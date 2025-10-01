de8a8463e02a749317356585e58347a1f3eaa5c9
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2025-10-01 12:14:30 +0530

    Time: 9 ms (5.30%) | Memory: 19.9 MB (8.36%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map <int, int> count;
-        vector<vector <int>> freq(nums.size() + 1);
-        
+        vector<vector<int>> freq(nums.size() + 1);
+
         for(int n : nums) {
             count[n] = 1 + count[n];
         }
         for(auto const& value : count) {
             freq[value.second].push_back(value.first);
         }
-        vector <int> result;
+        vector<int> result;
         for(int i = freq.size() - 1; i > 0; --i) {
             for(int n : freq[i]) {
                 result.push_back(n);
