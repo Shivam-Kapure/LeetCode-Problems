5f0d1e2936c1f544fcc5ca63a36c071e467144f4
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2025-10-06 19:51:30 +0530

    Time: 14 ms (11.92%) | Memory: 13.3 MB (37.53%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map <char, int> rome = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
-        {'C', 100}, {'D', 500}, {'M', 1000}};
+        unordered_map<char, int> rome = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100},
+        {'D', 500}, {'M', 1000}};
+
         int result = 0;
         for(int i = 0; i < s.length(); i++) {
             auto a = rome.find(s[i]);
             if(a != rome.end()) {
+                auto b = rome.find(s[i + 1]);
                 if(i + 1 < s.length()) {
-                    auto b = rome.find(s[i + 1]);
-                    if(a->second < b->second) {
+                    if(b != rome.end() && a->second < b->second) {
                         int c = b->second - a->second;
                         result = result + c;
                         ++i;
