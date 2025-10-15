1ff06f27bab938479c3d0a8c2bd32a1402fef4a9
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2025-10-15 16:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (76.13%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(); int n = matrix[0].size();
-        int row = 0; int col = n - 1;
-
-        while(row < m && col >= 0) {
-            if(matrix[row][col] > target) {
-                col--;
+    bool binarysearch(vector<int>& row, int target) {
+        int low = 0; int high = row.size() - 1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(row[mid] == target) {
+                return true;
             }
-            else if(matrix[row][col] < target) {
-                row++;
+            else if(row[mid] < target) {
+                low = mid + 1;
             }
             else {
-                return true;
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(); int n = matrix[0].size();
+        int startrow = 0; int endrow = m - 1;
+
+        while(startrow <= endrow) {
+            int midrow = startrow + (endrow - startrow)/2;
+            if(matrix[midrow][0] <= target && matrix[midrow][n - 1] >= target) {
+                return binarysearch(matrix[midrow], target);
+            }
+            else if(matrix[midrow][n - 1] < target) {
+                startrow = midrow + 1;
+            }
+            else {
+                endrow = midrow - 1;
             }
         }
         return false;
