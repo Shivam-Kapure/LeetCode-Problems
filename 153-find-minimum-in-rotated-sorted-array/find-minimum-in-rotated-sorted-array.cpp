fd69a304551fd2d0a3d9fa4cafe4c28065d75d1c
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2025-10-18 12:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (48.17%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -10,10 +10,10 @@ public:
         while(left < right) {
             int mid = left + (right - left)/2;
             if(nums[mid] > nums[right]) {
-                left = mid + 1;
+                left = mid + 1; //yaha pe we have excluded mid because ceratin case hai jaise ki [2,1] type ka vector
             }
             else {
-                right = mid;
+                right = mid; //because mid can also be min element
             } 
         }
         return nums[left];
