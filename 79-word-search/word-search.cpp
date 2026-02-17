bc2db73bb73fe6e7c657d04b5d05fc0317a88f7d
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2026-02-17 21:55:31 +0530

    Time: 831 ms (22.43%) | Memory: 11.9 MB (14.24%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int ROWS, COLS;
+    vector<vector<bool>> visited;
+
+    bool helperBT(vector<vector<char>>& board, string word, int r, int c, int idx) {
+        if(idx == word.length())
+            return true;
+
+        if(r < 0 || c < 0 || r >= ROWS || c >= COLS || visited[r][c] || board[r][c] != word[idx])
+            return false;
+        
+        visited[r][c] = true;
+        bool res = helperBT(board, word, r + 1, c, idx + 1) ||
+                   helperBT(board, word, r - 1, c, idx + 1) ||
+                   helperBT(board, word, r, c + 1, idx + 1) ||
+                   helperBT(board, word, r, c - 1, idx + 1);
+        visited[r][c] = false;
+
+        return res;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        ROWS = board.size(); COLS = board[0].size();
+        visited = vector<vector<bool>>(ROWS, vector<bool>(COLS, false));
+
+        for(int r = 0; r < ROWS; r++) {
+            for(int c = 0; c < COLS; c++) {
+                if(helperBT(board, word, r, c, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
