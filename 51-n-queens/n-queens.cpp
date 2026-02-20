a30911517f5fcd96bf2de1a44d082bf0b93aab20
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2026-02-20 22:45:20 +0530

    Time: 5 ms (22.33%) | Memory: 10 MB (99.61%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-    vector<int> dx = {-1, -1, -1};
-    vector<int> dy = {-1, 0, 1};
-    int d = dx.size();
-
-    bool isValid(vector<vector<char>> &board, int i, int j, int n) {
-        for (int k = 0; k < d; k++) {
-            int x = i + dx[k], y = j + dy[k];
-            while (x > -1 && x < n && y > -1 && y < n) {
-                if (board[x][y] == 'Q') {return false;}
-                x += dx[k];
-                y += dy[k];
-            }
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+        for(int i = 0; i < n; i++) {
+            if(board[row][i] == 'Q')
+                return false;
         }
+        
+        for(int i = 0; i < n; i++) {
+            if(board[i][col] == 'Q')
+                return false;
+        }
+
+        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        for(int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        
         return true;
     }
 
-    vector<string> construct(vector<vector<char>> &grid, int n) {
-        vector<string> ans;
-        for (int i = 0; i < n; i++) {
-            string line(grid[i].begin(), grid[i].end());
-            ans.push_back(line);
-        }
-        return ans;
-    }
-
-    void backtrack(vector<vector<string>> &ans, vector<vector<char>> &cur, int i, int n) {
-        if (i == n) {
-            ans.push_back(construct(cur, n));
+    void nQueens(vector<string>& board, vector<vector<string>>& ans, int row, int n) {
+        if(row == n) {
+            ans.push_back(board);
             return;
         }
-        for (int j = 0; j < n; j++) {
-            if (isValid(cur, i, j, n)) {
-                cur[i][j] = 'Q';
-                backtrack(ans, cur, i + 1, n);
-                cur[i][j] = '.';
+        for(int i = 0; i < n; i++) {
+            if(isSafe(board, row, i, n)) {
+                board[row][i] = 'Q';
+                nQueens(board, ans, row + 1, n);
+                board[row][i] = '.';
             }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
         vector<vector<string>> ans;
-        vector<vector<char>> temp(n, vector<char>(n, '.'));
-        backtrack(ans, temp, 0, n);
+        nQueens(board, ans, 0, n);
         return ans;
     }
 };
