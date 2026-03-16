d26c69c125437631d1fc99f2c41b7d801dcb940a
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2026-03-16 19:22:45 +0530

    Time: 2 ms (57.22%) | Memory: 28.1 MB (60.29%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int dfsHelper(vector<vector<int>>& grid, vector<vector<bool>>& visited, int r, int c, int i, int j) {
-        if(i < 0 || j < 0 || i >= r || j >= c || visited[i][j] == true || grid[i][j] != 1)
+    int dfsHelper(int i, int j, vector<vector<bool>>& visited, vector<vector<int>>& grid, int r, int c) {
+        if(i < 0 || j < 0 || i >= r || j >= c)
             return 0;
-
+        
+        if(visited[i][j] == true || grid[i][j] != 1)
+            return 0;
+        
         visited[i][j] = true;
 
-        return 1 + dfsHelper(grid, visited, r, c, i - 1, j) 
-                 + dfsHelper(grid, visited, r, c, i, j + 1) 
-                 + dfsHelper(grid, visited, r, c, i + 1, j) 
-                 + dfsHelper(grid, visited, r, c, i, j - 1);
+        return 1 + dfsHelper(i - 1, j, visited, grid, r, c)
+                 + dfsHelper(i, j + 1, visited, grid, r, c)
+                 + dfsHelper(i + 1, j, visited, grid, r, c)
+                 + dfsHelper(i, j - 1, visited, grid, r, c);
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int r = grid.size(); int c = grid[0].size();
-        vector<vector<bool>> visited(r, vector<bool>(c, false));
         int area = 0;
-        
+        int r = grid.size();
+        int c= grid[0].size();
+
+        vector<vector<bool>> visited(r, vector<bool>(c, false));
+
         for(int i = 0; i < r; i++) {
             for(int j = 0; j < c; j++) {
-                if(grid[i][j] == 1 && !visited[i][j]) {
-                    area = max(area, dfsHelper(grid, visited, r, c, i , j));
-                }
+                if(!visited[i][j] && grid[i][j] == 1) {
+                    area = max(area, dfsHelper(i, j, visited, grid, r, c));
+                } 
             }
         }
         return area;
