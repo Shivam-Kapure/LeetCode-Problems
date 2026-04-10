076646385c0788ce2ab8870fed08222cbcbd9fcf
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2026-04-10 20:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (73.23%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        if (nums.empty()) return res;
+
+        int start = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1] + 1) {
+                if (start == i - 1)
+                    res.push_back(to_string(nums[start]));
+                else
+                    res.push_back(to_string(nums[start]) + "->" + to_string(nums[i - 1]));
+                start = i;
+            }
+        }
+
+        if (start == nums.size() - 1)
+            res.push_back(to_string(nums[start]));
+        else
+            res.push_back(to_string(nums[start]) + "->" + to_string(nums.back()));
+
+        return res;
+    }
+};
