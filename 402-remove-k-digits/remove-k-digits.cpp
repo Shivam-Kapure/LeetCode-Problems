aac6f97f739413ad87787e98d3242375778dbc73
Shivam Kapure <149868742+Shivam-Kapure@users.noreply.github.com>
2026-06-12 19:15:52 +0530

    Time: 13 ms (7.03%) | Memory: 13.9 MB (5.41%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<char>st;
-        for(auto currDigit : num){
-            while(!st.empty() && k>0 && st.top()>currDigit){
+        stack<int> st;
+
+        for(auto n : num) {
+            while(!st.empty() && k > 0 && st.top() > n) {
                 st.pop();
                 k--;
             }
-            st.push(currDigit);
+            st.push(n);
         }
-        // element left
-        while(!st.empty() && k>0){
-                st.pop();
-                k--;
+
+        while(!st.empty() && k > 0) {
+            st.pop();
+            k--;
         }
-        // edge case 
-        if(st.empty()) return "0";
+
+        while(st.empty()) return "0";
+
         string ans = "";
-        while(!st.empty()){
+
+        while(!st.empty()) {
             ans.push_back(st.top());
             st.pop();
-        }
-        // trimming zeroes
-        while(ans.size()>0 && ans.back()=='0'){
+        }   
+
+        while(ans.size() > 0 && ans.back() == '0')
             ans.pop_back();
-        }
+        
         if(ans.empty()) return "0";
-        reverse(ans.begin(),ans.end());
+
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
